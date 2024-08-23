9a56c5ac8405c161d41c19d698cc38804e61f65a
Mohammad Sharaf <mohammad.m.sharaf@henkel.com>
2024-08-23 11:52:59 +0200

    added consumer pipelines simulations

---
+++ ConsumerService2/consumer_pipeline_simulation.sh
@@ -0,0 +1,19 @@
+#this is simulation of the consumer pipeline
+#it should run the tests and generate the pacts
+
+#we should copy all the pacts to the central pact repo, in form of git commit to the master branch
+#and then run all provider's pipelines to verify the pacts
+#if all the providers pass, then all good.
+#otherwise, we should fail the build, and revert the commit
+
+path_to_pacts="../CentralPactRepo/pacts"
+consumer="ConsumerService1"
+
+rm -rf pacts
+mkdir -p pacts
+
+#run the tests
+npm run test
+
+#copy the pacts to the central pact repo
+cp -R pacts/* $path_to_pacts/
